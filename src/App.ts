5d002bcc50011f0fa994b8f251a382fba7335a95
Evan Suma Rosenberg <suma@umn.edu>
2024-02-08 13:47:31 -0600

    pre-class commit

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,4 @@
-/* GopherGfx App Template
+/* Lecture 8
  * CSCI 4611, Spring 2024, University of Minnesota
  * Instructor: Evan Suma Rosenberg <suma@umn.edu>
  * License: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
