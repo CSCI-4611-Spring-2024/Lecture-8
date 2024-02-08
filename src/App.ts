3fe2519c105cdbb2fc3b87e10758b8aaaec7ce28
Evan Suma Rosenberg <suma@umn.edu>
2024-02-08 15:16:33 -0600

    class update

--- src/App.ts
+++ src/App.ts
@@ -9,6 +9,7 @@ import * as gfx from 'gophergfx'
 export class App extends gfx.GfxApp
 {
     private ground: gfx.Mesh3;
+    private skybox: gfx.Mesh3;
 
     // --- Create the App class ---
     constructor()
@@ -17,22 +18,40 @@ export class App extends gfx.GfxApp
         super();
 
         this.ground = gfx.Geometry3Factory.createBox(50, 1, 50);
+        this.skybox = gfx.Geometry3Factory.createBox(100, 100, 100);
     }
 
 
     // --- Initialize the graphics scene ---
     createScene(): void 
     {
+        // Setup the camera projection matrix and position.
+        // We will learn more about camera models later in this course.
         this.camera.setPerspectiveCamera(60, 1920/1080, 0.1, 100);
         this.camera.position.set(0, 1.5, 0);
 
+        // Create an ambient light that illuminates everything in the scene
         const ambientLight = new gfx.AmbientLight(new gfx.Color(0.4, 0.4, 0.4));
         
+        // Create a directional light that is infinitely far away (sunlight)
+        const directionalLight = new gfx.DirectionalLight(new gfx.Color(0.6, 0.6, 0.6));
+        directionalLight.position.set(1, 2, 1);
+
         this.ground.position.set(0, -.5, 0);
         this.ground.material.setColor(new gfx.Color(83/255, 209/255, 110/255));
 
+        this.skybox.material = new gfx.UnlitMaterial();
+        this.skybox.material.side = gfx.Side.BACK;
+        this.skybox.material.setColor(new gfx.Color(0.698, 1, 1));
+
+        const testSphere = gfx.Geometry3Factory.createSphere();
+        testSphere.position.set(0, 2, -6);
+
         this.scene.add(ambientLight);
+        this.scene.add(directionalLight);
         this.scene.add(this.ground);
+        this.scene.add(this.skybox);
+        this.scene.add(testSphere);
     }
 
     
